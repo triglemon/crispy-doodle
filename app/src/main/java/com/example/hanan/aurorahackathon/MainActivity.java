30fa350985ec30a165d0655e3383a4996d23842d
hanhan1280 <hanhan1280@gmail.com>
2019-04-22 11:08:44 -0400

    Initial Commit
    :)

---
+++ app/src/main/java/com/example/hanan/aurorahackathon/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hanan.aurorahackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
