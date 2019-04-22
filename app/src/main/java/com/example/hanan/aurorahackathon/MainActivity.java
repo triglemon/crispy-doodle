3b91edba2217f4e0dc620e428faaa2232f1b8f53
hanhan1280 <hanhan1280@gmail.com>
2019-04-22 12:33:54 -0400

    loginpage
    login ui/ux

--- app/src/main/java/com/example/hanan/aurorahackathon/MainActivity.java
+++ app/src/main/java/com/example/hanan/aurorahackathon/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.hanan.aurorahackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
